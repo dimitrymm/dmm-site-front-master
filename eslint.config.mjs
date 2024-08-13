ca7457b03d6351f209b8295350af7873fccfef84
Dimitry <dimitrymmarinho@gmail.com>
2024-08-13 19:42:44 -0300

    Configurando ESlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,11 +4,18 @@ import pluginReact from "eslint-plugin-react";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    extends:{
+      plugin:react/jsx-runtime,
+
+    }
+},
+
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
-  
 
-  
+
+
 ];
